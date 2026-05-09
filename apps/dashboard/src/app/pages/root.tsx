5838d5863d5b7f3d9abad5a99fac6779e78f25d0
Friedrich482 <friedricht109@gmail.com>
2026-05-09 23:36:14 +0200

    init: dashboard
    - created the vite+react router dashboard

---
+++ apps/dashboard/src/app/pages/root.tsx
@@ -0,0 +1,3 @@
+export const Root = () => {
+  return <main>root</main>;
+};
---
+++ apps/dashboard/src/app/router.tsx
@@ -0,0 +1,23 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import App from "./App";
+import { Root } from "./pages/root";
+
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Root />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
